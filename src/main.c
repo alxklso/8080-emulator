5e4c1485ab7cc66bedd6af92381d6c282ff57772
alxklso <ark594@nyu.edu>
2023-07-27 14:08:38 -0400

    file reading

--- src/main.c
+++ src/main.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 int main(int argc, char**argv) {
-    printf("test\n");
-    return 0;
+    
+    // File handling 
+    char file_path[] = "../files/invaders.h"; 
+    FILE *file_pointer = fopen(file_path, "r");
+
+    if (file_pointer == NULL) {
+        printf("ERROR: %s could not be opened.\n", file_path);
+        exit(1);
+    } 
+
+    // Read file contents into buffer 
+    fseek(file_pointer, 0L, SEEK_END); // Place pointer at end of file 
+    int file_size = ftell(file_pointer); // Get the location of the pointer (at the end)
+    fseek(file_pointer, 0L, SEEK_SET); // Place pointer back at start of file 
+
+    printf("SUCCESS: %s could be opened (size: %d bytes).\n", file_path, file_size);
+ 
+ 
+    exit(1);
 }
